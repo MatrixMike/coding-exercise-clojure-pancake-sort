39a9f4fd7579fbe423181f17c571dc796faeb3ba
Tushar Pokle <tushar.pokle@gmail.com>
2014-07-14 00:38:24 +1000

    Refactor - more sequences

--- src/pancake_sort/core.clj
+++ src/pancake_sort/core.clj
@@ -7,14 +7,12 @@
   (if (> 2 (count v))
     v
     (let [spatula       (inc (index-of-biggest v))
-          on-spatula    (subvec v 0 spatula)
-          under-spatula (subvec v spatula)
-          nv            (vec (reverse (into (vec (reverse on-spatula))
-                                            under-spatula)))]
+          on-spatula    (take spatula v)
+          under-spatula (drop spatula v)
+          nv            (vec (reverse (concat (reverse on-spatula)
+                                              under-spatula)))]
       (conj (pancake-sort (pop nv))
             (peek nv)))))
 
 
 
-(concat [1 2] [3])
-(reverse [1 2])
