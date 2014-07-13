a764c8de3ab5f3f1cc9f3f46e3b5142a3e2c4475
Tushar Pokle <tushar.pokle@gmail.com>
2014-07-14 00:21:40 +1000

    Refactored around the pivot point - the spatula

--- src/pancake_sort/core.clj
+++ src/pancake_sort/core.clj
@@ -2,12 +2,19 @@
 
 (defn biggest [v] (apply max v))
 (defn index-of-biggest [v] (.indexOf v (biggest v)))
-(defn last-index-of [v] (+ 1 (count v)))
 
 (defn pancake-sort [v]
   (if (> 2 (count v))
     v
-    (let [nv (vec (reverse (into
-                            (vec (reverse (subvec v 0 (inc (index-of-biggest v)))))
-                            (subvec v (inc (index-of-biggest v))))))]
-      (conj (pancake-sort (pop nv)) (peek nv)))))
+    (let [spatula       (inc (index-of-biggest v))
+          on-spatula    (subvec v 0 spatula)
+          under-spatula (subvec v spatula)
+          nv            (vec (reverse (into (vec (reverse on-spatula))
+                                            under-spatula)))]
+      (conj (pancake-sort (pop nv))
+            (peek nv)))))
+
+
+
+(concat [1 2] [3])
+(reverse [1 2])
