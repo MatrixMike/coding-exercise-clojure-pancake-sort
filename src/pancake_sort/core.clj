de0d8189e29b0d822c5cef0d2edb777b3ed0319f
Tushar Pokle <tushar.pokle@gmail.com>
2014-07-13 23:55:53 +1000

    Removed top and bottom check - they were redundant

--- src/pancake_sort/core.clj
+++ src/pancake_sort/core.clj
@@ -5,31 +5,9 @@
 (defn last-index-of [v] (+ 1 (count v)))
 
 (defn pancake-sort [v]
-  (cond
-
-   (empty? v) v
-
-   (= 1 (count v)) v
-
-   ; top of the stack
-   (= 0
-      (index-of-biggest v))
-   (pancake-sort (vec (reverse v)))
-
-   ; bottom of the stack
-   (= (last-index-of v)
-      (index-of-biggest v))
-   (into (pancake-sort (pop v)) (peek v))
-
-    ; middle of the stack
-    :else
-   (let [nv (vec (reverse (into
-                       (vec (reverse (subvec v 0 (inc (index-of-biggest v)))))
-                       (subvec v (inc (index-of-biggest v))))))]
-
-     (conj (pancake-sort (pop nv)) (peek nv))
-
-     )
-
-    )
-  )
+  (if (> 2 (count v))
+    v
+    (let [nv (vec (reverse (into
+                            (vec (reverse (subvec v 0 (inc (index-of-biggest v)))))
+                            (subvec v (inc (index-of-biggest v))))))]
+      (conj (pancake-sort (pop nv)) (peek nv)))))
